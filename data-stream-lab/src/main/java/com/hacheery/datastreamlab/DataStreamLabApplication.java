41ca463b573af1e48f2028265673eac199f1f88b
vanhach97 <vanhach97@gmail.com>
2025-06-07 08:16:17 +0700

    init project

---
+++ data-stream-lab/src/main/java/com/hacheery/datastreamlab/DataStreamLabApplication.java
@@ -0,0 +1,13 @@
+package com.hacheery.datastreamlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStreamLabApplication.class, args);
+	}
+
+}
---
+++ data-stream-lab/src/test/java/com/hacheery/datastreamlab/DataStreamLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacheery.datastreamlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStreamLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
